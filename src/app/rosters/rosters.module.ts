7d3e72d408e9b26c22a74329ae4502be0adb1ae1
Tim Buckland-Kerr <timbk86@gmail.com>
2017-09-16 18:28:36 +1000

    change rosters-routing to rosters.routing

--- src/app/rosters/rosters.module.ts
+++ src/app/rosters/rosters.module.ts
@@ -1,4 +1,4 @@
-import { RostersRoutingModule } from './rosters-routing.module';
+import { RostersRoutingModule } from './rosters.routing';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
