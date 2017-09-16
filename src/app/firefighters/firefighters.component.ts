5670a5c7f6c0c5b0130bb14d4bc9972f38a9605c
Tim Buckland-Kerr <timbk86@gmail.com>
2017-09-16 18:08:34 +1000

    add get ff call to ff component

--- src/app/firefighters/firefighters.component.ts
+++ src/app/firefighters/firefighters.component.ts
@@ -1,3 +1,4 @@
+import { DataStorageService } from './../shared/data-storage.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirefightersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataStorageService: DataStorageService) { }
 
   ngOnInit() {
+    this.dataStorageService.getFirefighters();
   }
 
 }
