f9ab15b5783162d82ef2b65f99a5af8292355983
Tim Buckland-Kerr <timbk86@gmail.com>
2018-05-09 17:16:50 +1000

    set up auth interceptor

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,12 @@
-import { SharedModule } from './../shared/shared.module';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { SharedModule } from './../shared/shared.module';
 import { AppRoutingModule } from './../app.routing';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
-import { NgModule } from '@angular/core';
+import { AuthInterceptor } from '../shared/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -20,6 +23,7 @@ import { NgModule } from '@angular/core';
     HeaderComponent
   ],
   providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
   ]
 })
 export class CoreModule {}
