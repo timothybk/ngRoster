d62bedabfc3f8f1e51adf89fe94c97f95a796582
Tim Buckland-Kerr <timbk86@gmail.com>
2018-05-11 21:06:40 +1000

    navigate with authguard if not authenticated

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -1,4 +1,4 @@
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 
@@ -8,13 +8,17 @@ import * as fromAuth from './store/auth.reducers';
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-  constructor(private store: Store<fromApp.AppState>) {}
+  constructor(private store: Store<fromApp.AppState>, private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     return this.store.select('auth')
       .take(1)
       .map((authState: fromAuth.State) => {
-      return authState.isAuthenticated;
+        if (authState.isAuthenticated) {
+          return true;
+        } else {
+          this.router.navigate(['/signin']);
+        }
       });
   }
 }
