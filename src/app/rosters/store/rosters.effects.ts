04ea773514d71e822e657af42cada2ffc3d0e1a0
Tim Buckland-Kerr <timbk86@gmail.com>
2018-09-06 19:52:39 +1000

    log result in front end

--- src/app/rosters/store/rosters.effects.ts
+++ src/app/rosters/store/rosters.effects.ts
@@ -46,6 +46,7 @@ export class RostersEffects {
         return this.httpClient.get<FfPumpTotal[]>('/api/ffpumptotals');
       })
       .map((data: FfPumpTotal[]) => {
+        console.log(data);
         return {
           type: RostersActions.SET_FF_PUMP_TOTALS,
           payload: data
