9210f3df847d7f3c05f4444ed05bd6ea779de256
timothybk <timbk86@gmail.com>
2019-03-29 01:02:24 +1100

    auth effects moved to pipe config

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,14 +1,12 @@
 import { Store } from '@ngrx/store';
-import { Actions, Effect } from '@ngrx/effects';
+import { Actions, Effect, ofType } from '@ngrx/effects';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpRequest, HttpEventType } from '@angular/common/http';
 import { Router } from '@angular/router';
 
 import { User } from './../user.model';
 
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/switchMap';
+import { map, switchMap } from "rxjs/operators";
 
 import * as fromApp from './../../store/app.reducer';
 import * as authActions from './auth.actions';
@@ -16,67 +14,67 @@ import * as authActions from './auth.actions';
 @Injectable()
 export class AuthEffects {
 
-  @Effect({dispatch: false})
-  authSignin = this.actions$
-  .ofType(authActions.TRY_SIGNIN)
-  .map((action: authActions.TrySignIn) => {
-    return action.payload;
-  })
-  .switchMap(data => {
-    const req = new HttpRequest(
-      'POST',
-      '/user/signin',
-      data
-    );
-    return this.httpClient.request(req);
-  })
-  .map((event => {
-    let token;
-    if (event.type === HttpEventType.Response) {
-      token = event.body['token'];
+  @Effect({ dispatch: false })
+  authSignin = this.actions$.pipe(
+    ofType(authActions.TRY_SIGNIN),
+    map((action: authActions.TrySignIn) => {
+      return action.payload;
+    }),
+    switchMap(data => {
+      const req = new HttpRequest(
+        'POST',
+        '/user/signin',
+        data
+      );
+      return this.httpClient.request(req);
+    }),
+    map((event => {
+      let token;
+      if (event.type === HttpEventType.Response) {
+        token = event.body['token'];
 
-      localStorage.setItem('token', token);
+        localStorage.setItem('token', token);
 
-      this.router.navigate(['/']);
-    }
-  }));
+        this.router.navigate(['/']);
+      }
+    })));
 
-  @Effect({dispatch: false})
-  authLogout = this.actions$
-  .ofType(authActions.LOGOUT)
+  @Effect({ dispatch: false })
+  authLogout = this.actions$.pipe(
+    ofType(authActions.LOGOUT))
   .do(() => {
-    localStorage.removeItem('token');
-    this.router.navigate(['/signin']);
-  });
+  localStorage.removeItem('token');
+  this.router.navigate(['/signin']);
+});
 
-  // ======old sign up=======
-  // there is no sign up now
+// ======old sign up=======
+// there is no sign up now
 
-  // @Effect({dispatch: false})
-  // authRegister = this.actions$
-  //   .ofType(authActions.TRY_SIGNUP)
-  //   .map((action: authActions.TrySignUp) => {
-  //     return action.payload;
-  //   })
-  //   .switchMap(data => {
-  //     const req = new HttpRequest(
-  //       'POST',
-  //       '/user/register',
-  //       data,
-  //       {
-  //         reportProgress: true
-  //       }
-  //     );
-  //     return this.httpClient.request(req);
-  //   })
-  //   .map((res) => {
-  //     this.router.navigate(['/']);
-  //   });
+// @Effect({dispatch: false})
+// authRegister = this.actions$
+//   .ofType(authActions.TRY_SIGNUP)
+//   .map((action: authActions.TrySignUp) => {
+//     return action.payload;
+//   })
+//   .switchMap(data => {
+//     const req = new HttpRequest(
+//       'POST',
+//       '/user/register',
+//       data,
+//       {
+//         reportProgress: true
+//       }
+//     );
+//     return this.httpClient.request(req);
+//   })
+//   .map((res) => {
+//     this.router.navigate(['/']);
+//   });
 
-  constructor(
-    private actions$: Actions,
-    private store: Store<fromApp.AppState>,
-    private httpClient: HttpClient,
-    private router: Router
-  ) {}
+constructor(
+  private actions$: Actions,
+  private store: Store < fromApp.AppState >,
+  private httpClient: HttpClient,
+  private router: Router
+) { }
 }
