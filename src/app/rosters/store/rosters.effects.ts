47af1b12edae05af5d2dbb5e2cf80489ef76b44a
timothybk <timbk86@gmail.com>
2019-03-29 01:10:39 +1100

    fixed rosters effects with pipe config

--- src/app/rosters/store/rosters.effects.ts
+++ src/app/rosters/store/rosters.effects.ts
@@ -4,11 +4,10 @@ import { HttpClient, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx';
 import { of } from 'rxjs';
 import { Store } from '@ngrx/store';
-import { Actions, Effect } from '@ngrx/effects';
+import { Actions, Effect, ofType } from '@ngrx/effects';
 import { Injectable } from '@angular/core';
 
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/map';
+import { map, switchMap } from "rxjs/operators";
 
 import * as fromApp from './../../store/app.reducer';
 import * as RostersActions from './rosters.actions';
@@ -20,51 +19,51 @@ import { Router } from '@angular/router';
 @Injectable()
 export class RostersEffects {
   @Effect()
-  updateN2 = this.actions$
-    .ofType(RostersActions.UPDATE_N2)
-    .map((action: RostersActions.UpdateN2) => {
+  updateN2 = this.actions$.pipe(
+    ofType(RostersActions.UPDATE_N2),
+    map((action: RostersActions.UpdateN2) => {
       return action.payload;
-    })
-    .switchMap(data => {
+    }),
+    switchMap(data => {
       const req = new HttpRequest(
         'POST',
         '/api/nightduty',
         data
       );
       return this.httpClient.request(req);
-    })
-    .map(() => {
+    }),
+    map(() => {
       return {
         type: FirefighterActions.FETCH_FIREFIGHTERS
       }
-    })
+    }));
 
     @Effect()
-    rostersFetchFfPumpTotals = this.actions$
-      .ofType(RostersActions.FETCH_FF_PUMP_TOTALS)
-      .switchMap((action: RostersActions.FetchFfPumpTotals) => {
+    rostersFetchFfPumpTotals = this.actions$.pipe(
+      ofType(RostersActions.FETCH_FF_PUMP_TOTALS),
+      switchMap((action: RostersActions.FetchFfPumpTotals) => {
         return this.httpClient.get<ShiftsApiResponse>('/api/ffpumptotals');
-      })
-      .map((data: ShiftsApiResponse) => {
+      }),
+      map((data: ShiftsApiResponse) => {
         console.log(data);
         return {
           type: RostersActions.SET_FF_PUMP_TOTALS,
           payload: data
         };
-      });
+      }));
 
     @Effect()
-    pumpsFetch = this.actions$
-      .ofType(RostersActions.FETCH_PUMPS)
-      .switchMap((action: RostersActions.FetchPumps) => {
+    pumpsFetch = this.actions$.pipe(
+      ofType(RostersActions.FETCH_PUMPS),
+      switchMap((action: RostersActions.FetchPumps) => {
         return this.httpClient.get<Pump[]>('/api/pumps');
-      })
-      .map((data: Pump[]) => {
+      }),
+      map((data: Pump[]) => {
         return {
           type: RostersActions.SET_PUMPS,
           payload: data
         };
-      });
+      }));
 
   constructor(
     private actions$: Actions,
