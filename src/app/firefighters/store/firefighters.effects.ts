4bf0c3a892d5717290c61b3d687b0bebeb86a469
timothybk <timbk86@gmail.com>
2019-03-29 01:07:27 +1100

    fixed firefighter effects with pipe config

--- src/app/firefighters/store/firefighters.effects.ts
+++ src/app/firefighters/store/firefighters.effects.ts
@@ -4,16 +4,15 @@ import { Store } from '@ngrx/store';
 import { Injectable } from '@angular/core';
 import { Qualification } from './../../shared/qualification.model';
 import { Firefighter } from './../../shared/firefighter.model';
-import { Effect, Actions } from '@ngrx/effects';
+import { Effect, Actions, ofType } from '@ngrx/effects';
+import { map, switchMap } from "rxjs/operators";
 
 import * as RostersActions from '../../rosters/store/rosters.actions';
 import * as firefighterActions from './firefighters.actions';
 import * as fromApp from '../../store/app.reducer';
 import * as fromFirefighters from '../store/firefighters.reducers';
 
-import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/withLatestFrom';
 import 'rxjs/add/operator/do';
@@ -22,25 +21,25 @@ import 'rxjs/add/operator/filter';
 @Injectable()
 export class FirefighterEffects {
   @Effect()
-  firefightersFetch = this.actions$
-    .ofType(firefighterActions.FETCH_FIREFIGHTERS)
-    .switchMap((action: firefighterActions.FetchFirefighters) => {
+  firefightersFetch = this.actions$.pipe(
+    ofType(firefighterActions.FETCH_FIREFIGHTERS),
+    switchMap((action: firefighterActions.FetchFirefighters) => {
       return this.httpClient.get<Firefighter>('/api/firefighters');
-    })
-    .map((data: Firefighter) => {
+    }),
+    map((data: Firefighter) => {
       return {
         type: firefighterActions.SET_FIREFIGHTERS,
         payload: data
       };
-    });
+    }));
 
   @Effect()
-  firefightersDelete = this.actions$
-    .ofType(firefighterActions.DELETE_FIREFIGHTER)
-    .map((action: firefighterActions.DeleteFirefighter) => {
+  firefightersDelete = this.actions$.pipe(
+    ofType(firefighterActions.DELETE_FIREFIGHTER),
+    map((action: firefighterActions.DeleteFirefighter) => {
       return action.payload;
-    })
-    .switchMap(ffNumber => {
+    }),
+    switchMap(ffNumber => {
       const req = new HttpRequest(
         'POST',
         '/api/deletefirefighter',
@@ -50,20 +49,20 @@ export class FirefighterEffects {
         }
       );
       return this.httpClient.request(req);
-    })
-    .map(() => {
+    }),
+    map(() => {
       return {
         type: firefighterActions.FETCH_FIREFIGHTERS
       };
-    });
+    }));
 
   @Effect()
-  firefighterStore = this.actions$
-    .ofType(firefighterActions.STORE_FIREFIGHTER)
-    .map((action: firefighterActions.StoreFirefighter) => {
+  firefighterStore = this.actions$.pipe(
+    ofType(firefighterActions.STORE_FIREFIGHTER),
+    map((action: firefighterActions.StoreFirefighter) => {
       return action.payload;
-    })
-    .switchMap(data => {
+    }),
+    switchMap(data => {
       const req = new HttpRequest(
         'POST',
         '/api/firefighter',
@@ -73,20 +72,20 @@ export class FirefighterEffects {
         }
       );
       return this.httpClient.request(req);
-    })
-    .map(() => {
+    }),
+    map(() => {
       return {
         type: firefighterActions.FETCH_FIREFIGHTERS
       };
-    });
+    }));
 
     @Effect()
-  firefighterUpdate = this.actions$
-    .ofType(firefighterActions.UPDATEDB_FIREFIGHTER)
-    .map((action: firefighterActions.UpdateDbFirefighter) => {
+  firefighterUpdate = this.actions$.pipe(
+    ofType(firefighterActions.UPDATEDB_FIREFIGHTER),
+    map((action: firefighterActions.UpdateDbFirefighter) => {
       return action.payload;
-    })
-    .switchMap(data => {
+    }),
+    switchMap(data => {
       const req = new HttpRequest(
         'POST',
         '/api/updatefirefighter',
@@ -96,12 +95,12 @@ export class FirefighterEffects {
         }
       );
       return this.httpClient.request(req);
-    })
-    .map(() => {
+    }),
+    map(() => {
       return {
         type: firefighterActions.FETCH_FIREFIGHTERS
       };
-    });
+    }));
 
   constructor(
     private actions$: Actions,
